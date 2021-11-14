fc84f43f90203b05fdd87ffde615277884ddf6b2
kkkzn <77775859+kkkzn@users.noreply.github.com>
2021-11-14 17:37:51 +0900

    try to configure heroku postgresql

--- remainder/config.py
+++ remainder/config.py
@@ -8,6 +8,6 @@ class Config:
     load_dotenv(dotenv_path)
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://localhost/remainder"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///remainder.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
