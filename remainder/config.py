3217b8527f2e712692d3e24c8b44905b5bfb7982
kkkzn <77775859+kkkzn@users.noreply.github.com>
2021-11-14 19:00:01 +0900

    sqlalchemy url replace

--- remainder/config.py
+++ remainder/config.py
@@ -9,5 +9,7 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://kazunorioda@127.0.0.1:5432/remainder"
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
